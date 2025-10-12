0120c6a4cc1cc355a0722d06596cd40c2ac13ffe
sangwookkhu <ksw090711@gmail.com>
2025-10-12 23:37:57 +0900

    백준 사칙연산 고양이(10171) 메모리: 13880 KB, 시간 96ms

---
+++ src/BAEKJOON/사칙연산/S10171.java
@@ -0,0 +1,20 @@
+package BAEKJOON.사칙연산;
+
+// 고양이
+// https://www.acmicpc.net/problem/10171
+
+/**
+ * \    /\
+ *  )  ( ')
+ * (  /  )
+ *  \(__)|
+ */
+
+public class S10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
