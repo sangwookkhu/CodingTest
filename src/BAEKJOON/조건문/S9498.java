7ef0ff4927c1aba527eac833dc04c5698ee0e3c7
sangwookkhu <ksw090711@gmail.com>
2025-10-12 23:57:10 +0900

    백준 9498

---
+++ src/BAEKJOON/조건문/S9498.java
@@ -0,0 +1,24 @@
+package BAEKJOON.조건문;
+
+// 시험 성적
+// https://www.acmicpc.net/problem/9498
+
+import java.util.Scanner;
+
+public class S9498 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int score = sc.nextInt();
+
+        if(score >= 90) System.out.println("A");
+        else if(score >= 80) System.out.println("B");
+        else if(score >= 70) System.out.println("C");
+        else if(score >= 60) System.out.println("D");
+        else System.out.println("F");
+
+        sc.close();
+    }
+}
+
+// 메모리: 17700KB, 시간: 176ms
