8571161b3a3bd31cdefde0882ca162821a50e07a
sangwookkhu <ksw090711@gmail.com>
2025-10-12 23:43:12 +0900

    백준 개(10172)

---
+++ src/BAEKJOON/사칙연산/S10172.java
@@ -0,0 +1,16 @@
+package BAEKJOON.사칙연산;
+
+// 개
+// https://www.acmicpc.net/problem/10172
+
+public class S10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
+
+// 메모리: 13912KB, 시간: 100ms
