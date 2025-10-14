24ed76d0561a7bc4aa5c271df03b0eb902a6cf5e
sangwookkhu <ksw090711@gmail.com>
2025-10-14 19:44:16 +0900

    백준 8393(메모리: 17684KB, 시간: 168ms)

---
+++ src/BAEKJOON/반복문/S8393.java
@@ -0,0 +1,23 @@
+package BAEKJOON.반복문;
+
+import java.util.Scanner;
+
+public class S8393 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= N; i++) {
+            sum += i;
+        }
+
+        System.out.println(sum);
+
+        sc.close();
+    }
+}
+
+// 메모리: 17684KB, 시간: 168ms
