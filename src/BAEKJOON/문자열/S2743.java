7635077563b826d3bd3bd1bb31ed9cb5f6ad2dd9
sangwookkhu <ksw090711@gmail.com>
2025-11-08 13:27:40 +0900

    백준 문자열 2743(메모리: 17640KB, 시간: 168ms)

---
+++ src/BAEKJOON/문자열/S2743.java
@@ -0,0 +1,18 @@
+package BAEKJOON.문자열;
+
+// 단어 길이 재기
+// https://www.acmicpc.net/problem/2743
+
+import java.util.Scanner;
+
+public class S2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        System.out.println(str.length());
+    }
+}
+
+// 메모리: 17640KB, 시간: 168ms
