540bbf314df86e7de152533ecf9fb821d04e0f25
sangwookkhu <ksw090711@gmail.com>
2025-12-30 09:54:46 +0900

    [Programmers] 편지

---
+++ src/Programmers/Level0/Solution26.java
@@ -0,0 +1,15 @@
+package Programmers.Level0;
+
+/*
+편지
+https://school.programmers.co.kr/learn/courses/30/lessons/120898
+ */
+
+public class Solution26 {
+    public int solution(String message) {
+        int answer = 0;
+
+        answer = message.length() * 2;
+        return answer;
+    }
+}
