292bc483df3b0d5ada4e5c0c30eb59fb9410e955
sangwookkhu <ksw090711@gmail.com>
2026-01-07 18:18:45 +0900

    [Programmers]Level0 머쓱이보다 키 큰 사람

--- src/Programmers/Level0/Solution30.java
+++ src/Programmers/Level0/Solution30.java
@@ -1,5 +1,10 @@
 package Programmers.Level0;
 
+/*
+머쓱이보다 키 큰 사람
+https://school.programmers.co.kr/learn/courses/30/lessons/120585
+ */
+
 public class Solution30 {
     public int solution(int[] array, int height) {
         int n = array.length;
