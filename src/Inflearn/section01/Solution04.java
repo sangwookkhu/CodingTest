9496586d0ac1807f3f57fbe45489475147a5e18c
sangwookkhu <ksw090711@gmail.com>
2026-02-16 19:38:13 +0900

    [Inflearn] section01.단어 뒤집기 sol2

--- src/Inflearn/section01/Solution04.java
+++ src/Inflearn/section01/Solution04.java
@@ -4,14 +4,36 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solution04 {
+    // sol 1
+//    public ArrayList<String> solution(int n, String[] str){
+//        ArrayList<String> answer = new ArrayList<>();
+//        for(String x : str){
+//            String tmp = new StringBuilder(x).reverse().toString();
+//            answer.add(tmp);
+//        }
+//        return answer;
+//    }
+
+    // sol 2
     public ArrayList<String> solution(int n, String[] str){
         ArrayList<String> answer = new ArrayList<>();
-        for(String x : str){
-            String tmp = new StringBuilder(x).reverse().toString();
+        for(String x : str) {
+            char[] c = x.toCharArray();
+            int lt = 0, rt = x.length() - 1;
+            while(lt < rt) {
+                char tmp = c[lt];
+                c[lt] = c[rt];
+                c[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(c);
             answer.add(tmp);
         }
+
         return answer;
     }
+
 
     public static void main(String[] args) {
         Solution04 s = new Solution04();
