4177858091d386c3db4d93d6537ebf0261bb89dc
sangwookkhu <ksw090711@gmail.com>
2026-02-16 19:31:52 +0900

    [Inflearn] section01. 단어 뒤집기

---
+++ src/Inflearn/section01/Solution04.java
@@ -0,0 +1,30 @@
+package Inflearn.section01;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution04 {
+    public ArrayList<String> solution(int n, String[] str){
+        ArrayList<String> answer = new ArrayList<>();
+        for(String x : str){
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution04 s = new Solution04();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] str = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            str[i] = sc.next();
+        }
+
+        for(String x : s.solution(n, str)) {
+            System.out.println(x);
+        }
+    }
+}
