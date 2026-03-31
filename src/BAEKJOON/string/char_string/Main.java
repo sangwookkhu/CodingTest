f21c400f31ef521723803c8c676ebc62297f11b1
sangwookkhu <ksw090711@gmail.com>
2026-03-31 12:11:52 +0900

    [백준]문자열. 문자와 문자열

---
+++ src/BAEKJOON/string/char_string/Main.java
@@ -0,0 +1,14 @@
+package BAEKJOON.string.char_string;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        int i = sc.nextInt();
+
+        System.out.println(str.charAt(i-1));
+    }
+}
