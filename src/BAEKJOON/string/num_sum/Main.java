dfcc8a328f4b8e0bd2303ce9142c35c199af5ec9
sangwookkhu <ksw090711@gmail.com>
2026-04-02 09:20:33 +0900

    [백준]문자열. 숫자의 합

---
+++ src/BAEKJOON/string/num_sum/Main.java
@@ -0,0 +1,23 @@
+package BAEKJOON.string.num_sum;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        sc.nextLine();
+
+        String numbers = sc.nextLine();
+
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            sum += numbers.charAt(i) - '0';
+        }
+
+        System.out.println(sum);
+        sc.close();
+    }
+}
