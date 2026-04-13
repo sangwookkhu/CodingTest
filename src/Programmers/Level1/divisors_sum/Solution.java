466c6da9cd9ec286ed836d547662cb0bcfdcc894
sangwookkhu <ksw090711@gmail.com>
2026-04-13 15:06:32 +0900

    [Programmers]Level1. 약수의 합

---
+++ src/Programmers/Level1/divisors_sum/Solution.java
@@ -0,0 +1,14 @@
+package Programmers.Level1.divisors_sum;
+
+public class Solution {
+    public int solution(int n) {
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if(n % i == 0) sum += i;
+        }
+
+        return sum;
+    }
+}
